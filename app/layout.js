d52cc437d875b8f8fd5b1956014d182de481d90c
saijami <saijami@unosimple.com>
2024-10-28 19:08:28 +0530

    cleark setup

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -10,19 +11,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        <main className="min-h-screen bg-gradient-to-b from-purple-100 to white">
-          {children}
-        </main>
-        {/* Footer  */}
-        <footer className="bg-purple-300 py-12">
-          <div className="container mx-auto px-4 text-center">
-            Made with 🖤 by Sai Jami
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-purple-100 to white">
+            {children}
+          </main>
+          {/* Footer  */}
+          <footer className="bg-purple-300 py-12">
+            <div className="container mx-auto px-4 text-center">
+              Made with 🖤 by Sai Jami
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
