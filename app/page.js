d961b7e6402b665006437cc5c6f70c2a0b2c4c78
saijamii <saijami38@gmail.com>
2024-10-31 11:19:10 +0530

    carousel / steps

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Testimonial from "@/components/testimonial";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -101,6 +102,30 @@ export default function Home() {
             );
           })}
         </div>
+      </div>
+      <div className="mb-24">
+        <h2 className="text-3xl text-center font-bold mb-12 text-blue-600">
+          What Our Users say
+        </h2>
+        <Testimonial />
+      </div>
+      <div className="mb-24">
+        <h2 className="text-3xl font-bold text-center mb-12 text-blue-600">
+          How It Works
+        </h2>
+      </div>
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {howItWorks.map((step, index) => (
+          <div key={index} className="text-center">
+            <div className="bg-blue-100 rounded-full w-16 h-16 flex items-center justify-center mx-auto mb-4">
+              <span className="text-blue-600 font-bold text-xl">
+                {index + 1}
+              </span>
+            </div>
+            <h3 className="font-semibold text-lg mb-2">{step.step}</h3>
+            <p className="text-gray-600">{step.description}</p>
+          </div>
+        ))}
       </div>
     </main>
   );
