ce3cc6539fe134eb02f6886bca82a01d1bcce5c1
saijamii <saijami38@gmail.com>
2024-10-31 13:27:55 +0530

    image changes

--- components/testimonial.jsx
+++ components/testimonial.jsx
@@ -19,28 +19,28 @@ const Testimonial = () => {
       role: "Marketing Manager",
       content:
         "Schedulrr has transformed how I manage my team's meetings. It's intuitive and saves us hours every week!",
-      image: "https://i.pravatar.cc/150?img=1",
+      image: "https://i.pravatar.cc/150?img=7",
     },
     {
       name: "David Lee",
       role: "Freelance Designer",
       content:
         "As a freelancer, Schedulrr helps me stay organized and professional. My clients love how easy it is to book time with me.",
-      image: "https://i.pravatar.cc/150?img=2",
+      image: "https://i.pravatar.cc/150?img=11",
     },
     {
       name: "Emily Chen",
       role: "Startup Founder",
       content:
         "Schedulrr streamlined our hiring process. Setting up interviews has never been easier!",
-      image: "https://i.pravatar.cc/150?img=3",
+      image: "https://i.pravatar.cc/150?img=9",
     },
     {
       name: "Michael Brown",
       role: "Sales Executive",
       content:
         "I've seen a 30% increase in my meeting bookings since using Schedulrr. It's a game-changer for sales professionals.",
-      image: "https://i.pravatar.cc/150?img=4",
+      image: "https://i.pravatar.cc/150?img=8",
     },
   ];
 
