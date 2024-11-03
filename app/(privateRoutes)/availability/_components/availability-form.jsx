3fdec2cef80b6dce8813a7b6ea8e87ccb9ba05bb
saijamii <saijami38@gmail.com>
2024-11-03 11:49:12 +0530

    AvailabilityForm /availabilitySchema

--- app/(privateRoutes)/availability/_components/availability-form.jsx
+++ app/(privateRoutes)/availability/_components/availability-form.jsx
@@ -1,6 +1,150 @@
 "use client";
+
+import { availabilitySchema } from "@/app/lib/validators";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { timeSlots } from "../data";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const AvailabilityForm = ({ intialData }) => {
-  return <div>AvailabilityForm</div>;
+  console.log(intialData);
+  const {
+    register,
+    handleSubmit,
+    control,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(availabilitySchema),
+    defaultValues: { ...intialData },
+  });
+  return (
+    <form>
+      {[
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+      ].map((day, index) => {
+        const isAvailable = watch(`${day}.isAvailable`);
+
+        return (
+          <div key={index} className="flex items-center space-x-4 mb-4">
+            <Controller
+              name={`${day}.isAvailable`}
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={(checked) => {
+                      setValue(`${day}.isAvailable`, checked);
+                      if (!checked) {
+                        setValue(`${day}.startTime`, "09:00");
+                        setValue(`${day}.endTime`, "17:00");
+                      }
+                    }}
+                  />
+                );
+              }}
+            />
+            <span className="capitalize w-24">{day}</span>
+            {isAvailable && (
+              <>
+                <Controller
+                  name={`${day}.startTime`}
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Select
+                        onValueChange={field.onChange}
+                        value={field.value}
+                      >
+                        <SelectTrigger className="w-32">
+                          <SelectValue placeholder="Start Time" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timeSlots.map((time) => {
+                            return (
+                              <SelectItem key={time} value={time}>
+                                {time}
+                              </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                    );
+                  }}
+                />
+                <span>to</span>
+                <Controller
+                  name={`${day}.endTime`}
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Select
+                        onValueChange={field.onChange}
+                        value={field.value}
+                      >
+                        <SelectTrigger className="w-32">
+                          <SelectValue placeholder="End TIme" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timeSlots.map((time) => {
+                            return (
+                              <SelectItem key={time} value={time}>
+                                {time}
+                              </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                    );
+                  }}
+                />
+                {errors[day]?.endTime && (
+                  <span className="text-red-500 text-sm ml-2">
+                    {errors[day].endTime.message}
+                  </span>
+                )}
+              </>
+            )}
+          </div>
+        );
+      })}
+      <div className="flex items-center space-x-4">
+        <span className=" w-48">Minimum gap before booking (minutes) :</span>
+        <Input
+          type="number"
+          {...register("timeGap", {
+            valueAsNumber: true,
+          })}
+          className="w-32"
+        />
+        {errors.timeGap && (
+          <span className="text-red-500 text-sm ml-2">
+            {errors.timeGap.message}
+          </span>
+        )}
+      </div>
+      <Button type="submit" className="mt-5">
+        Update Avaliabilty
+      </Button>
+    </form>
+  );
 };
 
 export default AvailabilityForm;
--- app/lib/validators.js
+++ app/lib/validators.js
@@ -23,3 +23,32 @@ export const eventSchema = z.object({
   duration: z.number().int().positive("Duration must be a positive number"),
   isPrivate: z.boolean(),
 });
+
+export const daySchema = z
+  .object({
+    isAvailable: z.boolean(),
+    startTIme: z.string().optional(),
+    endTIme: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (data.isAvailable) {
+        return data.startTIme < data.endTIme;
+      }
+      return true;
+    },
+    {
+      message: "End time must be more than start time",
+      path: ["endTime"],
+    }
+  );
+export const availabilitySchema = z.object({
+  monday: daySchema,
+  tuesday: daySchema,
+  wednesday: daySchema,
+  thursday: daySchema,
+  friday: daySchema,
+  saturday: daySchema,
+  sunday: daySchema,
+  timeGap: z.number().min(0, "time gap must be 0 or more minutes").int(),
+});
