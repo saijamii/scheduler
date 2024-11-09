4478236f86991be3099b16413b761fe9a041d5fe
saijami <saijami@unosimple.com>
2024-11-09 17:54:27 +0530

    retun changess

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 
 const AuthLayout = ({ children }) => {
-  return;
-  <div className="flex justify-center pt-20">
-    <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
-  </div>;
+  return (
+    <div className="flex justify-center pt-20">
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+    </div>
+  );
 };
 
 export default AuthLayout;
