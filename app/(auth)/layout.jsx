b1da794fc58260bf00d8a36f6bbf21251906c7ae
saijami <saijami@unosimple.com>
2024-11-09 17:57:16 +0530

    t

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }) => {
     <div className="flex justify-center pt-20">
       <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
     </div>
-  );
+  )
 };
 
 export default AuthLayout;
